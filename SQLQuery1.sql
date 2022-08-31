481159dcb9c06f2e541bbc5a440373c3fe7a2ed1
Semanur <46786005+semagolet@users.noreply.github.com>
2022-08-31 11:15:39 +0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -20,7 +20,7 @@ Select * from Products Where CategoryId in (1,2)
 
 Select Count (*) as [Urun Sayisi] from Products
 
-Select Count (ProductName) from Products  /*  count fonskiyonlarý NULL ifadelerini saymaz     */
+Select Count (ProductName) from Products  /*  count fonskiyonlarÃ½ NULL ifadelerini saymaz     */
 
 Select Min(UnitPrice) from Products
 
@@ -32,21 +32,21 @@ Select Sum(UnitPrice * Quantity) as Kazanc from [Order Details]
 
 Select Rand()
 
-Select distinct(Country) from Customers order by Country /* tekrar eden bir þey var mý */
+Select distinct(Country) from Customers order by Country /* tekrar eden bir Ã¾ey var mÃ½ */
 
 Select distinct Country, City from Customers order by Country
 
-/*hangi ülkede kaç tane müteri var */ 
+/*hangi Ã¼lkede kaÃ§ tane mÃ¼teri var */ 
 
-Select Country, Count(*) Adet from Customers group by Country /* her grup için count(*) çalýþtýrýlýr lþste tutmak gibi */
+Select Country, Count(*) Adet from Customers group by Country /* her grup iÃ§in count(*) Ã§alÃ½Ã¾tÃ½rÃ½lÃ½r lÃ¾ste tutmak gibi */
 
 Select Country,City,  Count(*) Adet from Customers 
 where City <> 'Nantes'
-group by Country, City having Count(*)>1  /* having kümülatif sorgular sýralamalariçin */ 
+group by Country, City having Count(*)>1  /* having kÃ¼mÃ¼latif sorgular sÃ½ralamalariÃ§in */ 
 order by Country
 
 Select * from Products inner join Categories 
-on Products.CategoryID = Categories.CategoryID /* iki tabloda þarta eþleþenleri getirir */
+on Products.CategoryID = Categories.CategoryID /* iki tabloda Ã¾arta eÃ¾leÃ¾enleri getirir */
 
 Select * from Products inner join Categories 
 on Products.CategoryID = Categories.CategoryID
@@ -76,3 +76,40 @@ on p.ProductID = o.ProductID
 
 select * from Products p left join [Order Details] o
 on p.ProductID = o.ProductID
+
+
+/* left join */
+
+Select * from Customers c left join Orders o
+on c.CustomerID = o.Customer.ID
+where o.CudtomerID is null
+
+
+Select * from Orders o right join Customers c
+on o.CustomerID = c.CustomerID
+where o.CustomerID is null
+
+Select * from Customers c full join Orders o
+on o.CustomerID = c.CustomerID
+
+Select p.ProductName, count(*) as Adet from Products p inner join [Order Details] o
+on p.ProductID= o.ProductID
+where o.Discount>0
+group by p.ProductName
+order by ProdcutName
+
+Select * from CustomersWork
+insert into CustomersWork (CustomerID, CompanyName,ContactName)
+select CustomerId, CompanyName, ContactName from Customers
+
+
+update Customers set CompanyName = CustomersWork.CompanyName
+from
+Customers inner join CustomersWork
+on Customers.CustomerID = CustomersWork.CustomerID
+where CustomersWork.CompanyName like 'Â½testÂ½'
+
+Select CCustomerID, CompantName,ContactName from Customers
+union all
+selevt * from CustomersWork
+
